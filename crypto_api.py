ac8bd5d5a705cac99325676bbd9f1aee9f128d15
prakatheesh-byte <prakatheeshn2@gmail.com>
2026-02-03 11:21:43 +0530

    Updated crypto_api.py
    optimised the encryption and decryption

--- crypto_api.py
+++ crypto_api.py
@@ -10,25 +10,32 @@ from dna_protein_core import dna_protein_encrypt, dna_protein_decrypt
 app = FastAPI(title="Lightweight Image Crypto API")
 
 @app.post("/encrypt")
-async def encrypt_image(file: UploadFile = File(...)):
+async def encrypt_image(
+    file: UploadFile = File(...),
+    dna_rounds: int = 1,
+    protein_rounds: int = 2,
+    r: float = 3.99,
+    x0: float = 0.7
+):
 
-    # 1. Open image as grayscale
     image = Image.open(file.file).convert("L")
     arr = np.array(image, dtype=np.uint8)
 
-    # 2. Encrypt
     flat = arr.flatten()
-    encrypted = dna_protein_encrypt(flat.astype(np.int16)).astype(np.uint8)
+    encrypted = dna_protein_encrypt(
+        flat,
+        dna_rounds,
+        protein_rounds,
+        r,
+        x0
+    )
 
-    # 3. Convert back to image
     encrypted_img = encrypted.reshape(arr.shape)
     enc_image = Image.fromarray(encrypted_img.astype(np.uint8))
 
-    # 4. Save encrypted image
     save_path = "encrypted.png"
     enc_image.save(save_path)
 
-    # 5. Return encrypted image file
     return FileResponse(
         path=save_path,
         media_type="image/png",
@@ -36,32 +43,37 @@ async def encrypt_image(file: UploadFile = File(...)):
     )
 
 @app.post("/decrypt")
-async def decrypt_image(file: UploadFile = File(...)):
+async def decrypt_image(
+    file: UploadFile = File(...),
+    dna_rounds: int = 1,
+    protein_rounds: int = 2,
+    r: float = 3.99,
+    x0: float = 0.7
+):
 
-    # 1. Open encrypted image
     image = Image.open(file.file).convert("L")
     arr = np.array(image, dtype=np.uint8)
 
-    # 2. Decrypt
     flat = arr.flatten()
-    decrypted = dna_protein_decrypt(flat.astype(np.int16)).astype(np.uint8)
+    decrypted = dna_protein_decrypt(
+        flat,
+        dna_rounds,
+        protein_rounds,
+        r,
+        x0
+    )
 
-    # 3. Convert back to image
     decrypted_img = decrypted.reshape(arr.shape)
-    dec_image = Image.fromarray(decrypted_img)
+    dec_image = Image.fromarray(decrypted_img.astype(np.uint8))
 
-    # 4. Save decrypted image
     save_path = "decrypted.png"
     dec_image.save(save_path)
 
-    # 5. Return decrypted image
     return FileResponse(
         path=save_path,
         media_type="image/png",
         filename="decrypted.png"
     )
-
-
 
 @app.get("/")
 def root():
